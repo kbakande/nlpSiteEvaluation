ad1c1c04ac9c771666f428bd767019624786e098
koakande <koakande@gmail.com>
2021-03-07 10:39:00 +0000

    added modification to js files

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";alert("I am existing")})();
+(()=>{var e={876:()=>{}},r={};function t(o){if(r[o])return r[o].exports;var a=r[o]={exports:{}};return e[o](a,a.exports,t),a.exports}t.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return t.d(r,{a:r}),r},t.d=(e,r)=>{for(var o in r)t.o(r,o)&&!t.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:r[o]})},t.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{"use strict";var e=t(876);console.log(e.handleSubmit),alert("I am existing")})()})();
