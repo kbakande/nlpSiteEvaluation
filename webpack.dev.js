54ca2b5b366b4c22001de73bf4f96532b0c0ba23
koakande <koakande@gmail.com>
2021-03-14 03:58:20 +0000

    added option <write to disk: true>

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -9,9 +9,12 @@ module.exports = {
     mode: "development",
     devtool: "source-map",
     entry: "./src/client/index.js",
-    // output: {
-    //     clean: true, // Clean the output directory before emit.
-    // },
+    devServer: {
+        writeToDisk: true
+        },
+    output: {
+        clean: true, // Clean the output directory before emit.
+    },
     module: {
         rules: [
             {
@@ -26,16 +29,16 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            // cleanOnceBeforeBuildPatterns: ['./js/build/*', './css/build/*'],
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
+        // new CleanWebpackPlugin({
+        //     // Simulate the removal of files
+        //     // cleanOnceBeforeBuildPatterns: ['./js/build/*', './css/build/*'],
+        //     dry: true,
+        //     // Write Logs to Console
+        //     verbose: true,
+        //     // Automatically remove all unused webpack assets on rebuild
+        //     cleanStaleWebpackAssets: true,
+        //     protectWebpackAssets: false
+        // }),
         new BundleAnalyzerPlugin()
     ]
 };
