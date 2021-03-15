cab5148cd36a90b2a7ffa4cf12a4a784a4f9b729
koakande <koakande@gmail.com>
2021-03-15 08:00:56 +0000

    point index.js to Client output dir for handlesubmit function

--- src/client/index.js
+++ src/client/index.js
@@ -5,8 +5,8 @@ import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
-// import { checkForName } from './js/nameChecker';
 
-// console.log(checkForName);
-
-alert("I am existing");
+export {
+    handleSubmit,
+    checkForName
+}
