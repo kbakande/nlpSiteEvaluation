b9c7de0b7cf20f8211b6f141c1973a5eed31cd02
koakande <koakande@gmail.com>
2021-03-15 15:23:45 +0000

    added minifying css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     mode: "production",
     entry: "./src/client/index.js",
@@ -8,7 +11,10 @@ module.exports = {
         clean: true, // Clean the output directory before emit.
         libraryTarget: 'var',
         library: 'Client'
- },
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
             {
@@ -19,13 +25,18 @@ module.exports = {
              {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 };
