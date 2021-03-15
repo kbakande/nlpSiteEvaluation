c27108406c1f2263fa3e666bf56d8abb72f06dd6
koakande <koakande@gmail.com>
2021-03-15 08:03:28 +0000

    added output dir for js files

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,12 +4,21 @@ const webpack = require("webpack");
 module.exports = {
     mode: "production",
     entry: "./src/client/index.js",
+    output: {
+        clean: true, // Clean the output directory before emit.
+        libraryTarget: 'var',
+        library: 'Client'
+ },
     module: {
         rules: [
             {
                 test: '/\.js$',
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+             {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
