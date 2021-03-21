103f7f9e8d475dacd9a0804858443d2cf37babfb
koakande <koakande@gmail.com>
2021-03-21 09:25:35 +0000

    import the workbox plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-// const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: "production",
@@ -40,6 +40,6 @@ module.exports = {
             filename: "index.html",
         }),
         new MiniCssExtractPlugin({ filename: "[name].css" }),
-        // new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW()
     ]
 };
